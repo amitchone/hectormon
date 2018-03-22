9a7fc87ecc97a9a59c051788a871f12605a2f9a3
Adam Mitchell <adamstuartmitchell@gmail.com>
2018-03-22 18:32:37 +0000

    Sudo install script

--- install/setup.sh
+++ install/setup.sh
@@ -1,10 +1,10 @@
 echo Installing required packages for Hectormon
 echo
 sudo apt-get install mysql-server libmysqlclient-dev
-pip install virtualenv
+sudo pip install virtualenv
 cd ..
 virtualenv venv
 . venv/bin/activate
-pip install -r install/requirements.txt
+sudo pip install -r install/requirements.txt
 echo
 echo Installation complete!
